be4f424b2a210555db413a52351a97f290d32895
minarvatechnologies-7 <minarvatechnologies@gmail.com>
2026-09-12 11:33:39 +0530

    feat: add sitemap foundation

---
+++ app/sitemap.ts
@@ -0,0 +1,2 @@
+import type { MetadataRoute } from "next";
+export default function sitemap(): MetadataRoute.Sitemap { return [{ url: "https://www.minarvatechnologies.com", lastModified: new Date() }]; }
